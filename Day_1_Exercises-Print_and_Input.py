45fd1b6306623b880ba3a63e4e60c9835ac5ba80
yzagayevskiy <157728196+yzagayevskiy@users.noreply.github.com>
2024-01-25 21:13:32 -0600

    Create Day_1_Exercises-Print_and_Input.py

---
+++ Day_1_Exercises-Print_and_Input.py
@@ -0,0 +1,10 @@
+# Day 1
+# Print and Input functions
+# Print string in console
+print("Hello world!")
+print("Hello world!\nHello world!")
+print("Hello " + "Yevgeniy")
+# Ask question, receive answer, and reply back
+print("Hello " + input("What is your name? ") + "!")
+# Print length of a user provided string in console
+print(len(input()))
