8a23bdd92bbeda048512f388c02b3f8dabb68ca4
yzagayevskiy <157728196+yzagayevskiy@users.noreply.github.com>
2024-03-22 16:39:04 -0500

    Create Day_21_Exercises-Class_Inheritance.py

---
+++ Day_21_Exercises-Class_Inheritance.py
@@ -0,0 +1,24 @@
+class Animal:
+    def __init__(self):
+        self.n_eyes = 2
+
+    def breathe(self):
+        print("Inhale, exhale.")
+
+
+class Fish(Animal):
+    def __init__(self):
+        super().__init__()
+
+    def breathe(self):
+        super().breathe()
+        print("Doing this under water.")
+
+    def swim(self):
+        print("Swimming.")
+
+
+nemo = Fish()
+nemo.swim()
+nemo.breathe()
+print(nemo.n_eyes)
